c265fa5fce05b96505031bedd6aee95361c688c0
Mykola-Rad <radmykola1789@gmail.com>
2025-08-18 20:37:40 +0300

    refactored tests

--- Streetcode/Streetcode.XUnitTest/BLL/MediatR/Media/Art/GetById/GetArtByIdHandlerTests.cs
+++ Streetcode/Streetcode.XUnitTest/BLL/MediatR/Media/Art/GetById/GetArtByIdHandlerTests.cs
@@ -1,11 +1,11 @@
-﻿using AutoMapper;
+﻿using System.Linq.Expressions;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore.Query;
 using Moq;
 using Streetcode.BLL.DTO.Media.Art;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.BLL.MediatR.Media.Art.GetById;
 using Streetcode.DAL.Repositories.Interfaces.Base;
-using System.Linq.Expressions;
 using Xunit;
 using ArtEntity = Streetcode.DAL.Entities.Media.Images.Art;
 
@@ -45,9 +45,9 @@ namespace Streetcode.XUnitTest.BLL_Tests.MediatR.Media.Art.GetById
                 .Setup(r => r.ArtRepository.GetFirstOrDefaultAsync(
                     It.IsAny<Expression<Func<ArtEntity, bool>>>(),
                     It.IsAny<Func<IQueryable<ArtEntity>, IIncludableQueryable<ArtEntity, object>>>()))
-                .ReturnsAsync((Expression<Func<ArtEntity, bool>> predicate, Func<IQueryable<ArtEntity>, IIncludableQueryable<ArtEntity, object>> _) =>
-                    arts.AsQueryable().FirstOrDefault(predicate.Compile())
-                );
+                .ReturnsAsync((Expression<Func<ArtEntity, bool>> predicate, Func<IQueryable<ArtEntity>,
+                    IIncludableQueryable<ArtEntity, object>> _) =>
+                    arts.AsQueryable().FirstOrDefault(predicate.Compile()));
 
             _mapperMock.Setup(m => m.Map<ArtDTO>(targetArt)).Returns(targetArtDto);
 
@@ -60,14 +60,14 @@ namespace Streetcode.XUnitTest.BLL_Tests.MediatR.Media.Art.GetById
             Assert.True(result.IsSuccess);
             Assert.Equal(targetArtDto, result.Value);
 
-            _repositoryWrapperMock.Verify(r => r.ArtRepository.GetFirstOrDefaultAsync(
+            _repositoryWrapperMock.Verify(
+                r => r.ArtRepository.GetFirstOrDefaultAsync(
                 It.IsAny<Expression<Func<ArtEntity, bool>>>(),
                 It.IsAny<Func<IQueryable<ArtEntity>, IIncludableQueryable<ArtEntity, object>>>()), Times.Once);
 
             _mapperMock.Verify(m => m.Map<ArtDTO>(targetArt), Times.Once);
             _loggerMock.VerifyNoOtherCalls();
         }
-
 
         [Fact]
         public async Task Handle_WhenArtDoesNotExist_ReturnsFailAndLogsError()
@@ -85,9 +85,9 @@ namespace Streetcode.XUnitTest.BLL_Tests.MediatR.Media.Art.GetById
                 .Setup(r => r.ArtRepository.GetFirstOrDefaultAsync(
                     It.IsAny<Expression<Func<ArtEntity, bool>>>(),
                     It.IsAny<Func<IQueryable<ArtEntity>, IIncludableQueryable<ArtEntity, object>>>()))
-                .ReturnsAsync((Expression<Func<ArtEntity, bool>> predicate, Func<IQueryable<ArtEntity>, IIncludableQueryable<ArtEntity, object>> _) =>
-                    arts.AsQueryable().FirstOrDefault(predicate.Compile())
-                );
+                .ReturnsAsync((Expression<Func<ArtEntity, bool>> predicate, Func<IQueryable<ArtEntity>,
+                    IIncludableQueryable<ArtEntity, object>> _) =>
+                    arts.AsQueryable().FirstOrDefault(predicate.Compile()));
 
             var query = new GetArtByIdQuery(nonExistentArtId);
             var expectedMessage = $"Cannot find an art with corresponding id: {nonExistentArtId}";
@@ -99,15 +99,14 @@ namespace Streetcode.XUnitTest.BLL_Tests.MediatR.Media.Art.GetById
             Assert.False(result.IsSuccess);
             Assert.Equal(expectedMessage, result.Errors[0].Message);
 
-            _repositoryWrapperMock.Verify(r => r.ArtRepository.GetFirstOrDefaultAsync(
+            _repositoryWrapperMock.Verify(
+                r => r.ArtRepository.GetFirstOrDefaultAsync(
                 It.IsAny<Expression<Func<ArtEntity, bool>>>(),
                 It.IsAny<Func<IQueryable<ArtEntity>, IIncludableQueryable<ArtEntity, object>>>()), Times.Once);
 
             _mapperMock.Verify(m => m.Map<ArtDTO>(It.IsAny<ArtEntity>()), Times.Never);
             _loggerMock.Verify(l => l.LogError(query, expectedMessage), Times.Once);
         }
-
-
 
         [Fact]
         public async Task Handle_WhenArtIsNull_ReturnsFailAndLogsError()
