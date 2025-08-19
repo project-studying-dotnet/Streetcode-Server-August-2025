217d5d7afd92ebffbd769f809e5ed9bf6a509824
DeorixProg <ma2005ksi@gmail.com>
2025-08-19 19:57:07 +0300

    Made a standard structure for program.cs in DbUpdate

--- build/Build.cs
+++ build/Build.cs
@@ -1,23 +1,44 @@
+using System;
+using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
 
-namespace Targets;
-
-[ShutdownDotNetAfterServerBuild]
-partial class Build : NukeBuild
+class Build : NukeBuild
 {
-    public static int Main() => Execute<Build>(x => x.Setup);
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    Target Run => _ => _
-        .DependsOn(CompileAPI, CompileCli);
+    public static int Main () => Execute<Build>(x => x.Compile);
 
-    Target Setup => _ => _
-        .DependsOn(SetupPublic, SetupLocal);
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    Target Push => _ => _
-        .DependsOn(PushAll);
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+        });
 
-    Target Test => _ => _
-        .DependsOn(UnitTest, IntegrationTest)
-        .Triggers(EndAll);
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+        });
+
 }
