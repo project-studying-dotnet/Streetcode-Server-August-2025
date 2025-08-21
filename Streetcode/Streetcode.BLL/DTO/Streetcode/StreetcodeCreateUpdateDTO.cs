60a12b909b800a6ca6e2f48d2da1ddd92c8df253
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-08-21 19:54:04 +0300

    feature/#63: Refactor DTO classes for inheritance and cleanup

---
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/StreetcodeCreateUpdateDTO.cs
@@ -0,0 +1,22 @@
+﻿using Streetcode.BLL.DTO.Media.Images;
+using Streetcode.DAL.Enums;
+
+namespace Streetcode.BLL.DTO.Streetcode;
+
+public class StreetcodeCreateUpdateDTO
+{
+    public int Index { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string Title { get; set; } = null!;
+    public StreetcodeType StreetcodeType { get; set; }
+    public string? Alias { get; set; }
+    public string TransliterationUrl { get; set; } = null!;
+    public DateTime EventStartOrPersonBirthDate { get; set; }
+    public DateTime? EventEndOrPersonDeathDate { get; set; }
+    public string DateString { get; set; } = null!;
+    public string? Teaser { get; set; }
+    public StreetcodeStatus Status { get; set; }
+    public int? AudioId { get; set; }
+    public IEnumerable<ImageDetailsDto> ImagesDetails { get; set; } = new List<ImageDetailsDto>();
+}
