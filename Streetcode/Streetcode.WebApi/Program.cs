b9dc9f95cef7fc5092facc998cb45c76e57340f9
DeorixProg <ma2005ksi@gmail.com>
2025-08-21 20:13:28 +0300

    restore top-statment in program.cs

--- Streetcode/Streetcode.WebApi/Program.cs
+++ Streetcode/Streetcode.WebApi/Program.cs
@@ -3,63 +3,61 @@ using Streetcode.BLL.Services.BlobStorageService;
 using Streetcode.WebApi.Extensions;
 using Streetcode.WebApi.Utils;
 
-namespace Streetcode.WebApi
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.ConfigureApplication();
+
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddSwaggerServices();
+builder.Services.AddCustomServices();
+builder.Services.ConfigureBlob(builder);
+builder.Services.ConfigurePayment(builder);
+builder.Services.ConfigureInstagram(builder);
+builder.Services.ConfigureSerilog(builder);
+
+var app = builder.Build();
+
+if (app.Environment.EnvironmentName == "Local")
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Host.ConfigureApplication();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+}
+else
+{
+    app.UseHsts();
+}
 
-            builder.Services.AddApplicationServices(builder.Configuration);
-            builder.Services.AddSwaggerServices();
-            builder.Services.AddCustomServices();
-            builder.Services.ConfigureBlob(builder);
-            builder.Services.ConfigurePayment(builder);
-            builder.Services.ConfigureInstagram(builder);
-            builder.Services.ConfigureSerilog(builder);
-            var app = builder.Build();
+await app.ApplyMigrations();
 
-            if (app.Environment.EnvironmentName == "Local")
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-            }
-            else
-            {
-                app.UseHsts();
-            }
+// await app.SeedDataAsync();
+app.UseCors();
+app.UseHttpsRedirection();
+app.UseRouting();
 
-            await app.ApplyMigrations();
+app.UseAuthentication();
+app.UseAuthorization();
 
-            // await app.SeedDataAsync();
-            app.UseCors();
-            app.UseHttpsRedirection();
-            app.UseRouting();
+app.UseHangfireDashboard("/dash");
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+if (app.Environment.EnvironmentName != "Local")
+{
+    BackgroundJob.Schedule<WebParsingUtils>(
+        wp => wp.ParseZipFileFromWebAsync(), TimeSpan.FromMinutes(1));
 
-            app.UseHangfireDashboard("/dash");
+    RecurringJob.AddOrUpdate<WebParsingUtils>(
+        "WebParsingUtils_ParseZipFile",
+        wp => wp.ParseZipFileFromWebAsync(),
+        Cron.Monthly);
 
-            if (app.Environment.EnvironmentName != "Local")
-            {
-                BackgroundJob.Schedule<WebParsingUtils>(
-                    wp => wp.ParseZipFileFromWebAsync(), TimeSpan.FromMinutes(1));
-                RecurringJob.AddOrUpdate<WebParsingUtils>(
-                    "WebParsingUtils_ParseZipFile",
-                    wp => wp.ParseZipFileFromWebAsync(),
-                    Cron.Monthly);
-                RecurringJob.AddOrUpdate<BlobService>(
-                    "BlobService_CleanBlobStorage",
-                    b => b.CleanBlobStorage(),
-                    Cron.Monthly);
-            }
+    RecurringJob.AddOrUpdate<BlobService>(
+        "BlobService_CleanBlobStorage",
+        b => b.CleanBlobStorage(),
+        Cron.Monthly);
+}
 
-            app.MapControllers();
+app.MapControllers();
 
-            app.Run();
-        }
-    }
+app.Run();
+
+public partial class Program
+{
 }
