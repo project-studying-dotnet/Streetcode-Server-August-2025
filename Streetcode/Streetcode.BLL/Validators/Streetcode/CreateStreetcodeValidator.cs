99d25ca78a7e2380addfcacbbffb3d1549507845
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-08-22 12:08:43 +0300

    feature/#63: Add validators for creating and updating streetcodes

---
+++ Streetcode/Streetcode.BLL/Validators/Streetcode/CreateStreetcodeValidator.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using Streetcode.BLL.DTO.Streetcode.Create;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.Validators.Streetcode;
+
+public class CreateStreetcodeValidator : AbstractValidator<StreetcodeCreateDTO>
+{
+    private readonly IRepositoryWrapper _repositoryWrapper;
+
+    public CreateStreetcodeValidator(IRepositoryWrapper repositoryWrapper, BaseStreetcodeValidator baseStreetcodeValidator)
+    {
+        _repositoryWrapper = repositoryWrapper;
+
+        RuleFor(dto => dto).SetValidator(baseStreetcodeValidator);
+
+        RuleFor(dto => dto.Index)
+            .MustAsync(BeUniqueIndex).WithMessage("Index must be unique.");
+
+        RuleFor(dto => dto.ImagesDetails)
+            .NotEmpty()
+            .WithMessage("At least one image detail is required.");
+
+        RuleForEach(dto => dto.ImagesDetails.Select(x => x.ImageId))
+            .MustAsync(HasExistingImage)
+            .WithMessage("One or more images do not exist.");
+    }
+
+    private async Task<bool> BeUniqueIndex(int index, CancellationToken cancellationToken)
+    {
+        var existingStreetcode = await _repositoryWrapper.StreetcodeRepository.GetFirstOrDefaultAsync(sc => sc.Index == index);
+
+        return existingStreetcode == null;
+    }
+
+    private async Task<bool> HasExistingImage(int imageId, CancellationToken token)
+    {
+        var image = await _repositoryWrapper.ImageRepository.GetFirstOrDefaultAsync(i => i.Id == imageId);
+
+        return image != null;
+    }
+}
---
+++ Streetcode/Streetcode.BLL/Validators/Streetcode/UpdateStreetcodeValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Streetcode.BLL.DTO.Streetcode.Update;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.Validators.Streetcode
+{
+    public class UpdateStreetcodeValidator : AbstractValidator<StreetcodeUpdateDTO>
+    {
+        private readonly IRepositoryWrapper _repositoryWrapper;
+
+        public UpdateStreetcodeValidator(IRepositoryWrapper repositoryWrapper, BaseStreetcodeValidator baseStreetcodeValidator)
+        {
+            _repositoryWrapper = repositoryWrapper;
+
+            RuleFor(dto => dto).SetValidator(baseStreetcodeValidator);
+
+            RuleFor(dto => dto)
+                .MustAsync(BeUniqueIndex).WithMessage("Index must be unique.");
+        }
+
+        private async Task<bool> BeUniqueIndex(StreetcodeUpdateDTO streetcode, CancellationToken cancellationToken)
+        {
+            var existingStreetcode = await _repositoryWrapper.StreetcodeRepository
+                .GetFirstOrDefaultAsync(sc => sc.Index == streetcode.Index && sc.Id != streetcode.Id);
+
+            return existingStreetcode == null;
+        }
+    }
+}
