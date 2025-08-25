90064f0bfb0536cffc4a55c0049e4d1ae892765d
andrii <andriy6062002@gmail.com>
2025-08-26 01:16:40 +0300

    Implemented specification pattern. Added specifications for team and video.

---
+++ Streetcode/Streetcode.DAL/Specifications/Video/VideoByStreetCodeIdSpecification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.Specification;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Streetcode.DAL.Specifications.Video
+{
+    public class VideoByStreetCodeIdSpecification : Specification<Entities.Media.Video>
+    {
+        public VideoByStreetCodeIdSpecification(int streetcodeId)
+        {
+            Query.Where(v => v.StreetcodeId == streetcodeId);
+        }
+    }
+}
