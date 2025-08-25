9b425e188a961466a5601f7b50683397025f8831
DeorixProg <ma2005ksi@gmail.com>
2025-08-25 18:57:35 +0300

    complite delete of fact

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Fact/Delete/DeleteFactHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Fact.Delete
+{
+    public class DeleteFactHandler : IRequestHandler<DeleteFactCommand, Result<Unit>>
+    {
+        private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly ILoggerService _logger;
+
+        public DeleteFactHandler(IRepositoryWrapper repositoryWrapper, ILoggerService logger)
+        {
+            _repositoryWrapper = repositoryWrapper;
+            _logger = logger;
+        }
+
+        public async Task<Result<Unit>> Handle(DeleteFactCommand request, CancellationToken cancellationToken)
+        {
+            int id = request.id;
+            var fact = await _repositoryWrapper.FactRepository.GetFirstOrDefaultAsync(f => f.Id == id);
+            if (fact == null)
+            {
+                string errorMsg = $"No fact found by entered Id - {id}";
+                _logger.LogError(request, errorMsg);
+                return Result.Fail(errorMsg);
+            }
+
+            if (fact.Image is not null)
+            {
+                _repositoryWrapper.ImageRepository.Delete(fact.Image);
+            }
+
+            _repositoryWrapper.FactRepository.Delete(fact);
+            var resultIsSuccess = await _repositoryWrapper.SaveChangesAsync() > 0;
+            if (resultIsSuccess)
+            {
+                return Result.Ok(Unit.Value);
+            }
+            else
+            {
+                string errorMsg = "Failed to delete fact";
+                _logger.LogError(request, errorMsg);
+                return Result.Fail(new Error(errorMsg));
+            }
+        }
+    }
+}
--- Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/FactController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/FactController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
 using Streetcode.BLL.MediatR.Streetcode.Fact.Create;
+using Streetcode.BLL.MediatR.Streetcode.Fact.Delete;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetAll;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetById;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetByStreetcodeId;
@@ -31,5 +32,11 @@ public class FactController : BaseApiController
     public async Task<IActionResult> Create(int streetcodeId, [FromBody] FactDto newFact)
     {
         return HandleResult(await Mediator.Send(new CreateFactCommand(streetcodeId, newFact)));
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        return HandleResult(await Mediator.Send(new DeleteFactCommand(id)));
     }
 }
