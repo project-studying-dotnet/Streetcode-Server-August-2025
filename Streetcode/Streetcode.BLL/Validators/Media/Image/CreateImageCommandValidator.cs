569c24f59d3708532421ac0578faab101367399c
denys-pavskyi <den.pavski@gmail.com>
2025-08-26 03:24:57 +0300

    feature/52: Added validation for Media/Image - Create

---
+++ Streetcode/Streetcode.BLL/Validators/Media/Image/CreateImageCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Streetcode.BLL.MediatR.Media.Image.Create;
+
+namespace Streetcode.BLL.Validators.Media.Image;
+
+public class CreateImageCommandValidator : AbstractValidator<CreateImageCommand>
+{
+    public CreateImageCommandValidator()
+    {
+        RuleFor(x => x.Image)
+            .NotNull()
+            .WithMessage("Image data is required.");
+
+        When(x => x.Image != null, () =>
+        {
+            RuleFor(x => x.Image)
+                .SetValidator(new ImageFileBaseCreateDTOValidator());
+        });
+    }
+}
