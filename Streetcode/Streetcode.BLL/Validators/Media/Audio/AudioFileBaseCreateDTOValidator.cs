5599a876cedf75a84fda92defd1f9c5cea96680d
denys-pavskyi <den.pavski@gmail.com>
2025-08-26 19:11:52 +0300

    refactor/52: Added FileBaseCreateDTOValidator instead of two simmilar classes. Minor improvements

--- Streetcode/Streetcode.BLL/Validators/Media/Audio/AudioFileBaseCreateDTOValidator.cs
+++ Streetcode/Streetcode.BLL/Validators/Media/Audio/AudioFileBaseCreateDTOValidator.cs
@@ -3,31 +3,11 @@ using Streetcode.BLL.DTO.Media.Audio;
 
 namespace Streetcode.BLL.Validators.Media.Audio;
 
-public class AudioFileBaseCreateDTOValidator : AbstractValidator<AudioFileBaseCreateDTO>
+public class AudioFileBaseCreateDTOValidator
+    : FileBaseCreateDTOValidator<AudioFileBaseCreateDTO>
 {
     public AudioFileBaseCreateDTOValidator()
     {
-        RuleFor(x => x.Title)
-            .MaximumLength(100)
-            .WithMessage("Title cannot exceed 100 characters.")
-            .When(x => !string.IsNullOrWhiteSpace(x.Title));
-
-        RuleFor(x => x.MimeType)
-            .NotEmpty()
-            .WithMessage("MimeType is required.")
-            .MaximumLength(10)
-            .WithMessage("MimeType cannot exceed 10 characters.");
-
-        RuleFor(x => x.Extension)
-            .MaximumLength(10)
-            .WithMessage("Extension cannot exceed 10 characters.")
-            .When(x => !string.IsNullOrWhiteSpace(x.Extension));
-
-        RuleFor(x => x.BaseFormat)
-            .MaximumLength(20)
-            .WithMessage("Base format cannot exceed 20 characters.")
-            .When(x => !string.IsNullOrWhiteSpace(x.BaseFormat));
-
         RuleFor(x => x.Description)
             .MaximumLength(500)
             .WithMessage("Description cannot exceed 500 characters.")
---
+++ Streetcode/Streetcode.BLL/Validators/Media/FileBaseCreateDTOValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Streetcode.BLL.DTO.Media;
+
+namespace Streetcode.BLL.Validators.Media;
+
+public class FileBaseCreateDTOValidator<T> : AbstractValidator<T>
+    where T : FileBaseCreateDTO
+{
+    public FileBaseCreateDTOValidator()
+    {
+        RuleFor(x => x.Title)
+            .MaximumLength(100)
+            .WithMessage("Title cannot exceed 100 characters.")
+            .When(x => !string.IsNullOrWhiteSpace(x.Title));
+
+        RuleFor(x => x.MimeType)
+            .NotEmpty()
+            .WithMessage("MimeType is required.")
+            .MaximumLength(10)
+            .WithMessage("MimeType cannot exceed 10 characters.");
+
+        RuleFor(x => x.Extension)
+            .MaximumLength(10)
+            .WithMessage("Extension cannot exceed 10 characters.")
+            .When(x => !string.IsNullOrWhiteSpace(x.Extension));
+
+        RuleFor(x => x.BaseFormat)
+            .MaximumLength(20)
+            .WithMessage("Base format cannot exceed 20 characters.")
+            .When(x => !string.IsNullOrWhiteSpace(x.BaseFormat));
+    }
+}
