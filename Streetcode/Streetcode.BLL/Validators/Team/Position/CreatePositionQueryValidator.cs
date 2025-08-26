97fd260e1abc10e9f01905d15bd2755465376582
denys-pavskyi <den.pavski@gmail.com>
2025-08-26 17:26:59 +0300

    feature/52: Added validation for Team/Position - Create. Added validation for Team/TeamMemberLinks - Create

---
+++ Streetcode/Streetcode.BLL/Validators/Team/Position/CreatePositionQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Streetcode.BLL.MediatR.Team.Create;
+
+namespace Streetcode.BLL.Validators.Team.Position;
+
+public class CreatePositionQueryValidator : AbstractValidator<CreatePositionQuery>
+{
+    public CreatePositionQueryValidator()
+    {
+        RuleFor(x => x.position)
+            .NotNull()
+            .WithMessage("Position object is required.");
+
+        RuleFor(x => x.position.Position)
+            .NotEmpty()
+            .WithMessage("Position name is required.")
+            .MaximumLength(50)
+            .WithMessage("Position name cannot exceed 50 characters.");
+    }
+}
---
+++ Streetcode/Streetcode.BLL/Validators/Team/TeamMembersLinks/CreateTeamLinkQueryValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using Streetcode.BLL.MediatR.Team.TeamMembersLinks.Create;
+
+namespace Streetcode.BLL.Validators.Team.TeamMembersLinks;
+
+public class CreateTeamLinkQueryValidator : AbstractValidator<CreateTeamLinkQuery>
+{
+    public CreateTeamLinkQueryValidator()
+    {
+        RuleFor(x => x.teamMember)
+            .NotNull()
+            .WithMessage("Team member link data is required.");
+
+        When(x => x.teamMember != null, () =>
+        {
+            RuleFor(x => x.teamMember.LogoType)
+                .IsInEnum()
+                .WithMessage("LogoType must be a valid value.");
+
+            RuleFor(x => x.teamMember.TargetUrl)
+                .NotEmpty()
+                .WithMessage("Target URL is required.")
+                .MaximumLength(255)
+                .WithMessage("Target URL cannot exceed 255 characters.")
+                .Must(BeValidUrl)
+                .WithMessage("Target URL must be a valid absolute URL.");
+
+            RuleFor(x => x.teamMember.TeamMemberId)
+                .GreaterThan(0)
+                .WithMessage("TeamMemberId must be greater than 0.");
+        });
+    }
+
+    private static bool BeValidUrl(string url)
+    {
+        return Uri.TryCreate(url, UriKind.Absolute, out _);
+    }
+}
