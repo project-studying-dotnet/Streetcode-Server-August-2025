351956d65866f407aff0c8bd05e3c3dc4514baf2
Vladymor <vladusmoroz@gmail.com>
2025-08-27 08:50:54 +0300

    Moved EF configurations to separate classes

---
+++ Streetcode/Streetcode.DAL/Persistence/Configurations/StreetcodeTagIndexEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Streetcode.DAL.Entities.AdditionalContent;
+
+namespace Streetcode.DAL.Persistence.Configurations
+{
+    public class StreetcodeTagIndexEntityConfiguration : IEntityTypeConfiguration<StreetcodeTagIndex>
+    {
+        public void Configure(EntityTypeBuilder<StreetcodeTagIndex> builder)
+        {
+            builder.HasKey(nameof(StreetcodeTagIndex.StreetcodeId), nameof(StreetcodeTagIndex.TagId));
+        }
+    }
+}
