49ffbadcac07986f3a76fb6f293629076b80d85d
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-08-29 19:06:23 +0300

    feature/#110: Add art slide and creation update DTOs

---
+++ Streetcode/Streetcode.BLL/DTO/Media/Art/StreetcodeArtCreateUpdateDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Streetcode.BLL.DTO.Media.Art;
+
+public class StreetcodeArtCreateUpdateDTO
+{
+    public int Index { get; set; }
+    public int ArtId { get; set; }
+}
--- Streetcode/Streetcode.BLL/DTO/Streetcode/StreetcodeCreateUpdateDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/StreetcodeCreateUpdateDTO.cs
@@ -1,4 +1,6 @@
-﻿using Streetcode.BLL.DTO.Media.Images;
+﻿using Streetcode.BLL.DTO.ArtGallery;
+using Streetcode.BLL.DTO.Media.Art;
+using Streetcode.BLL.DTO.Media.Images;
 using Streetcode.DAL.Enums;
 
 namespace Streetcode.BLL.DTO.Streetcode;
@@ -26,4 +28,6 @@ public class StreetcodeCreateUpdateDTO
     public StreetcodeStatus Status { get; set; }
     public int? AudioId { get; set; }
     public IEnumerable<ImageDetailsDto> ImagesDetails { get; set; } = new List<ImageDetailsDto>();
+    public IEnumerable<ArtCreateUpdateDTO> Arts { get; set; } = new List<ArtCreateUpdateDTO>();
+    public IEnumerable<StreetcodeArtSlideCreateUpdateDTO> StreetcodeArtSlides { get; set; } = new List<StreetcodeArtSlideCreateUpdateDTO>();
 }
