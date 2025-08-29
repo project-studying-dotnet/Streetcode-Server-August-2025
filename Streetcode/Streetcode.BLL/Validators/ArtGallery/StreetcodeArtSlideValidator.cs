be5409b9a3d67b5abec994cf0908452b5aaf527b
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-08-29 19:47:41 +0300

    feature/#110: Add new validators

---
+++ Streetcode/Streetcode.BLL/Validators/ArtGallery/StreetcodeArtSlideValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Streetcode.BLL.DTO.ArtGallery;
+
+namespace Streetcode.BLL.Validators.ArtGallery;
+
+public class StreetcodeArtSlideValidator : AbstractValidator<StreetcodeArtSlideCreateUpdateDTO>
+{
+    public StreetcodeArtSlideValidator()
+    {
+        RuleFor(x => x.Template)
+            .IsInEnum()
+            .WithMessage("Invalid Template value.");
+
+        RuleForEach(x => x.StreetcodeArts)
+            .NotEmpty()
+            .WithMessage("StreetcodeArts collection cannot be empty.");
+    }
+}
