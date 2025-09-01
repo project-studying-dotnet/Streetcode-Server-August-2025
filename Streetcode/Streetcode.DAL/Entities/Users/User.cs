bea6af063f30ff10d3fa0589a6a4e00adf1c09f9
denys-pavskyi <den.pavski@gmail.com>
2025-09-01 16:57:46 +0300

    feat: Added RefreshToken and RefreshTokenExpiryTime to User, created migration

--- Streetcode/Streetcode.DAL/Entities/Users/User.cs
+++ Streetcode/Streetcode.DAL/Entities/Users/User.cs
@@ -27,5 +27,8 @@ namespace Streetcode.DAL.Entities.Users
         public string Password { get; set; }
         [Required]
         public UserRole Role { get; set; }
+
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
     }
 }
---
+++ Streetcode/Streetcode.DAL/Persistence/Migrations/20250901135656_AddRefreshTokenToUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streetcode.DAL.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                schema: "Users",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                schema: "Users",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                schema: "Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                schema: "Users",
+                table: "Users");
+        }
+    }
+}
