1f22bd9ad1cac5d4f04460a691f31bd262276cec
Mykola-Rad <radmykola1789@gmail.com>
2025-09-01 13:48:42 +0300

    Created DTO's and mapping

---
+++ Streetcode/Streetcode.BLL/DTO/Timeline/TimelineItem/TimelineItemCreateDTO.cs
@@ -0,0 +1,14 @@
+﻿using Streetcode.BLL.DTO.Timeline.HistoricalContext;
+using Streetcode.DAL.Enums;
+
+namespace Streetcode.BLL.DTO.Timeline.TimelineItem
+{
+    public class TimelineItemCreateDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public DateViewPattern DateViewPattern { get; set; }
+        public IEnumerable<HistoricalContextRequestDTO> HistoricalContexts { get; set; }
+    }
+}
--- Streetcode/Streetcode.BLL/Mapping/Timeline/TimelineItemProfile.cs
+++ Streetcode/Streetcode.BLL/Mapping/Timeline/TimelineItemProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
-using Streetcode.BLL.DTO.Timeline;
+using Streetcode.BLL.DTO.Timeline.HistoricalContext;
+using Streetcode.BLL.DTO.Timeline.TimelineItem;
 using Streetcode.DAL.Entities.Timeline;
 
 namespace Streetcode.BLL.Mapping.Timeline;
@@ -16,6 +17,28 @@ public class TimelineItemProfile : Profile
                 {
                     Id = x.HistoricalContextId,
                     Title = x.HistoricalContext.Title
+                }).ToList()));
+
+        CreateMap<TimelineItemCreateDTO, TimelineItem>()
+            .ForMember(dest => dest.HistoricalContextTimelines, opt => opt.MapFrom(src => src.HistoricalContexts
+                .Select(hc => new HistoricalContextTimeline
+                {
+                    HistoricalContext = new HistoricalContext
+                    {
+                        Id = hc.Id ?? 0,
+                        Title = hc.Title
+                    }
+                }).ToList()));
+
+        CreateMap<TimelineItemUpdateDTO, TimelineItem>()
+            .ForMember(dest => dest.HistoricalContextTimelines, opt => opt.MapFrom(src => src.HistoricalContexts
+                .Select(hc => new HistoricalContextTimeline
+                {
+                    HistoricalContext = new HistoricalContext
+                    {
+                        Id = hc.Id ?? 0,
+                        Title = hc.Title
+                    }
                 }).ToList()));
     }
 }
