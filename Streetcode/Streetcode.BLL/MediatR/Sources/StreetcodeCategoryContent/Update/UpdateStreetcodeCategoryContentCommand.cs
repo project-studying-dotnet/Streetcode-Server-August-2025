55b3b206eeea417ca08dfd7b6e9dcfe3d773fc57
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-09-02 23:02:19 +0300

    refactor/#125: Refactor command classes and validators for consistency

--- Streetcode/Streetcode.BLL/MediatR/Sources/StreetcodeCategoryContent/Update/UpdateStreetcodeCategoryContentCommand.cs
+++ Streetcode/Streetcode.BLL/MediatR/Sources/StreetcodeCategoryContent/Update/UpdateStreetcodeCategoryContentCommand.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentResults;
+﻿using FluentResults;
 using MediatR;
 using Streetcode.BLL.DTO.Sources;
 
 namespace Streetcode.BLL.MediatR.Sources.StreetcodeCategoryContent.Update
 {
-    public record UpdateStreetcodeCategoryContentCommand(CategoryContentUpdateDTO categoryContentUpdateDTO) : IRequest<Result<StreetcodeCategoryContentDTO>>;
+    public record UpdateStreetcodeCategoryContentCommand(CategoryContentUpdateDTO CategoryContentUpdateDTO)
+        : IRequest<Result<StreetcodeCategoryContentDTO>>;
 }
