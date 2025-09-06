7754c47e686444f1bab737df0181eda1777ae12e
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-09-06 21:41:47 +0300

    feature/#93: Refactor error handling in partner handler classes

--- Streetcode/Streetcode.BLL/MediatR/Partners/GetAll/GetAllPartnersHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Partners/GetAll/GetAllPartnersHandler.cs
@@ -2,10 +2,10 @@
 using FluentResults;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Streetcode.BLL.DTO.AdditionalContent.Subtitles;
 using Streetcode.BLL.DTO.Partners;
 using Streetcode.BLL.Interfaces.Logging;
-using Streetcode.DAL.Entities.AdditionalContent.Coordinates;
+using Streetcode.BLL.Resources;
+using Streetcode.BLL.Util.Extensions;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.MediatR.Partners.GetAll;
@@ -34,7 +34,7 @@ public class GetAllPartnersHandler : IRequestHandler<GetAllPartnersQuery, Result
 
         if (partners is null)
         {
-            const string errorMsg = $"Cannot find any partners";
+            string errorMsg = Errors_Common.NotFoundAny.FormatWith("partners");
             _logger.LogError(request, errorMsg);
             return Result.Fail(new Error(errorMsg));
         }
