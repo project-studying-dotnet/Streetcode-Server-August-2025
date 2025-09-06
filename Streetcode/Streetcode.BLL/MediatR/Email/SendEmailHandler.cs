d88f07c6c9643a6702cfd7de92d98abf3abb6c9e
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-09-06 17:42:07 +0300

    feature/#93: Add localized error messages for email sending

--- Streetcode/Streetcode.BLL/MediatR/Email/SendEmailHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Email/SendEmailHandler.cs
@@ -2,6 +2,7 @@ using FluentResults;
 using MediatR;
 using Streetcode.BLL.Interfaces.Email;
 using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.Resources;
 using Streetcode.DAL.Entities.AdditionalContent.Email;
 
 namespace Streetcode.BLL.MediatR.Email
@@ -22,13 +23,13 @@ namespace Streetcode.BLL.MediatR.Email
             var message = new Message(new string[] { "streetcodeua@gmail.com" }, request.Email.From, "FeedBack", request.Email.Content);
             bool isResultSuccess = await _emailService.SendEmailAsync(message);
 
-            if(isResultSuccess)
+            if (isResultSuccess)
             {
                 return Result.Ok(Unit.Value);
             }
             else
             {
-                const string errorMsg = $"Failed to send email message";
+                string errorMsg = Errors_Email.FailedToSend;
                 _logger.LogError(request, errorMsg);
                 return Result.Fail(new Error(errorMsg));
             }
