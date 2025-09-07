3a120f7c768aeae5d0a8057f92de9d1b13e24081
denys-pavskyi <den.pavski@gmail.com>
2025-09-07 22:28:07 +0300

    test: Added NewsControllerTests

---
+++ Streetcode/Streetcode.XUnitTest/WebApi/Controllers/NewsControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Streetcode.WebApi.Controllers;
+
+namespace Streetcode.XUnitTest.WebApi.Controllers;
+
+public class NewsControllerTests
+{
+    private readonly Mock<IMediator> _mediatorMock;
+    private readonly NewsController _controller;
+
+    public NewsControllerTests()
+    {
+        _mediatorMock = new Mock<IMediator>();
+
+        var httpContext = new DefaultHttpContext();
+        var serviceProvider = new Mock<IServiceProvider>();
+        serviceProvider
+            .Setup(sp => sp.GetService(typeof(IMediator)))
+            .Returns(_mediatorMock.Object);
+
+        httpContext.RequestServices = serviceProvider.Object;
+
+        _controller = new NewsController
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            }
+        };
+    }
+
+}
