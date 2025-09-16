6614238d8183ffbdc1b5700ba2008eba03be5f74
Valentyn Humen <valentin34561@gmail.com>
2025-09-16 19:29:49 +0300

    Create controller

--- Streetcode/Streetcode.WebApi/Controllers/Auth/AuthController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Auth/AuthController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.DTO.Users;
+using Streetcode.BLL.DTO.Users.Logout;
+using Streetcode.BLL.MediatR.Users.Logout;
 using Streetcode.BLL.MediatR.Users.Register;
 
 namespace Streetcode.WebApi.Controllers.Auth
@@ -14,6 +16,19 @@ namespace Streetcode.WebApi.Controllers.Auth
         public async Task<ActionResult<RegisterUserResponseDTO>> Register([FromBody] RegisterUserDTO dto, CancellationToken ct)
         {
             var result = await Mediator.Send(new RegisterUserCommand(dto), ct);
+
+            if (result.IsFailed)
+            {
+                return BadRequest(result.Errors.Select(e => e.Message));
+            }
+
+            return Ok(result.Value);
+        }
+
+        [HttpPost("logout")]
+        public async Task<ActionResult<LogoutResponceDTO>> Logout([FromBody] LogoutRequestDTO dto, CancellationToken ct)
+        {
+            var result = await Mediator.Send(new LogoutUserCommand(dto), ct);
 
             if (result.IsFailed)
             {
