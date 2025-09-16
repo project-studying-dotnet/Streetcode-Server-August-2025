d003549e95b51b95123bf4df8444349e9b7fb6df
denys-pavskyi <den.pavski@gmail.com>
2025-09-16 19:56:44 +0300

    feat: Added DeleteCommentCommand and DeleteCommentHandler

---
+++ Streetcode/Streetcode.BLL/MediatR/Comments/Delete/DeleteCommentHandler.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Comments;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Newss.Delete;
+using Streetcode.BLL.Resources;
+using Streetcode.BLL.Util.Extensions;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Comments.Delete;
+
+public class DeleteCommentCommandHandler
+    : IRequestHandler<DeleteCommentCommand, Result<CommentDTO>>
+{
+
+    private readonly IRepositoryWrapper _repositoryWrapper;
+    private readonly ILoggerService _logger;
+    private readonly IMapper _mapper;
+    public DeleteCommentCommandHandler(IRepositoryWrapper repositoryWrapper, ILoggerService logger, IMapper mapper)
+    {
+        _repositoryWrapper = repositoryWrapper;
+        _logger = logger;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<CommentDTO>> Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
+    {
+        int id = request.id;
+        var comment = await _repositoryWrapper.CommentRepository.GetFirstOrDefaultAsync(c => c.Id == id);
+        if (comment == null)
+        {
+            string errorMsg = Errors_Common.NotFoundById.FormatWith("comment", id);
+            _logger.LogError(request, errorMsg);
+            return Result.Fail(errorMsg);
+        }
+
+        _repositoryWrapper.CommentRepository.Delete(comment);
+        var resultIsSuccess = await _repositoryWrapper.SaveChangesAsync() > 0;
+        if (resultIsSuccess)
+        {
+            var mappedComment = _mapper.Map<CommentDTO>(comment);
+            return Result.Ok(mappedComment);
+        }
+        else
+        {
+            string errorMsg = Errors_Common.FailedToDelete.FormatWith("comment");
+            _logger.LogError(request, errorMsg);
+            return Result.Fail(new Error(errorMsg));
+        }
+    }
+
+}
