135e7a945b3a9a8552023786217a69270d8daf74
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2025-09-16 19:19:51 +0300

    feature/#175: Implement Create method in CommentController for comment creation

--- Streetcode/Streetcode.WebApi/Controllers/Comments/CommentController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Comments/CommentController.cs
@@ -1,5 +1,17 @@
-﻿namespace Streetcode.WebApi.Controllers.Comments;
+﻿using Microsoft.AspNetCore.Mvc;
+using Streetcode.BLL.DTO.Comments;
+using Streetcode.BLL.MediatR.Comments.Create;
+using Streetcode.DAL.Enums;
+using Streetcode.WebApi.Attributes;
 
-public class CommentController
+namespace Streetcode.WebApi.Controllers.Comments;
+
+public class CommentController : BaseApiController
 {
+    [HttpPost]
+    [AuthorizeRoles]
+    public async Task<IActionResult> Create([FromBody] CommentCreateDTO commentCreate)
+    {
+        return HandleResult(await Mediator.Send(new CreateCommentCommand(commentCreate)));
+    }
 }
