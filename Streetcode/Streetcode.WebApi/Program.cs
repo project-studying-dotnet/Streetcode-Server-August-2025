c5698d214329a976da85d16021f326b1a1358393
Oleksandr Hofner <oleksandrhofner@gmail.com>
2025-09-16 19:09:22 +0200

    feat/#179/setup api gateway ocelot to project (#199)
    * feat/#179/setup api gateway ocelot to project
    
    * added ocelot.json to publish with the project
    
    * fix link to file

--- Streetcode/Streetcode.WebApi/Program.cs
+++ Streetcode/Streetcode.WebApi/Program.cs
@@ -3,6 +3,8 @@ using Streetcode.BLL.Services.BlobStorageService;
 using Streetcode.WebApi.BackgroundServices;
 using Streetcode.WebApi.Extensions;
 using Streetcode.WebApi.Middlewares;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.ConfigureApplication();
@@ -16,6 +18,13 @@ builder.Services.ConfigureInstagram(builder);
 builder.Services.ConfigureSerilog(builder);
 builder.Services.ConfigureJwt(builder);
 builder.Services.AddHostedService<RefreshTokenCleanupService>();
+
+// Ocelot Basic setup
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddOcelot(); // single ocelot.json file in read-only mode
+builder.Services
+    .AddOcelot(builder.Configuration);
 
 // Connect extension method Identity
 builder.Services.AddIdentityServices();
@@ -64,6 +73,8 @@ if (app.Environment.EnvironmentName != "Local")
 
 app.MapControllers();
 
+// Add middlewares ocelot
+await app.UseOcelot();
 await app.RunAsync();
 
 public partial class Program
