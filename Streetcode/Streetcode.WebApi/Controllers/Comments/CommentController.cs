e07601609ab2c46bcbad6ed57bf8a65e8329bd83
denys-pavskyi <den.pavski@gmail.com>
2025-09-22 22:06:28 +0300

    refactor: Replaced (ReviewComment, BlockComment, UnblockComment) on (ApproveComment, RestrictComment)

--- Streetcode/Streetcode.WebApi/Controllers/Comments/CommentController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Comments/CommentController.cs
@@ -3,12 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.DTO.Comments;
 using Streetcode.BLL.MediatR.Comments.Create;
 using Streetcode.BLL.MediatR.Comments.Delete;
-using Streetcode.BLL.MediatR.Comments.ReviewComment;
-using Streetcode.BLL.MediatR.Comments.SetCommentBlockStatus;
 using Streetcode.BLL.MediatR.Comments.GetByStreetcodeId;
-using Streetcode.WebApi.Attributes;
+using Streetcode.BLL.MediatR.Comments.SetCommentRestrictedStatus;
 using Streetcode.BLL.MediatR.Comments.Update;
 using Streetcode.DAL.Enums;
+using Streetcode.WebApi.Attributes;
 using Streetcode.WebApi.Attributes;
 using Streetcode.WebApi.Utils;
 
@@ -55,39 +54,27 @@ public class CommentController : BaseApiController
         return HandleResult(await Mediator.Send(new UpdateCommentCommand(comment, userId)));
     }
 
-    [AuthorizeRoles(UserRole.MainAdministrator, UserRole.Administrator, UserRole.Moderator)]
-    [HttpPost("{id:int}/review")]
+    [Authorize(Roles = "Administrator,Moderator")]
+    [HttpPost("{id:int}/approve")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    public async Task<IActionResult> ReviewComment([FromRoute] int id, CancellationToken ct)
+    public async Task<IActionResult> ApproveComment([FromRoute] int id, CancellationToken ct)
     {
-        var result = await Mediator.Send(new ReviewCommentCommand(id), ct);
+        var result = await Mediator.Send(new SetCommentRestrictedStatusCommand(id, false), ct);
         return HandleResult(result);
     }
 
-    [AuthorizeRoles(UserRole.MainAdministrator, UserRole.Administrator, UserRole.Moderator)]
-    [HttpPost("{id:int}/block")]
+    [Authorize(Roles = "Administrator,Moderator")]
+    [HttpPost("{id:int}/restrict")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    public async Task<IActionResult> BlockComment([FromRoute] int id, CancellationToken ct)
+    public async Task<IActionResult> RestrictComment([FromRoute] int id, CancellationToken ct)
     {
-        var result = await Mediator.Send(new SetCommentBlockStatusCommand(id, true), ct);
-        return HandleResult(result);
-    }
-
-    [AuthorizeRoles(UserRole.MainAdministrator, UserRole.Administrator, UserRole.Moderator)]
-    [HttpPost("{id:int}/unblock")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    public async Task<IActionResult> UnblockComment([FromRoute] int id, CancellationToken ct)
-    {
-        var result = await Mediator.Send(new SetCommentBlockStatusCommand(id, false), ct);
+        var result = await Mediator.Send(new SetCommentRestrictedStatusCommand(id, true), ct);
         return HandleResult(result);
     }
 }
