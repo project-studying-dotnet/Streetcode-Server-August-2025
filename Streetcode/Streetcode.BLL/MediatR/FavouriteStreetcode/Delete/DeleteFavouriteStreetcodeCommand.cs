b99596e403f14b49b981b01adc99dfdb7ed3f227
denys-pavskyi <den.pavski@gmail.com>
2025-09-22 23:37:23 +0300

    feat: Added command and hanled for DeleteFavouriteStreetcode

---
+++ Streetcode/Streetcode.BLL/MediatR/FavouriteStreetcode/Delete/DeleteFavouriteStreetcodeCommand.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.FavouriteStreetcode;
+using Streetcode.DAL.Enums;
+
+namespace Streetcode.BLL.MediatR.FavouriteStreetcode.Delete;
+
+public record DeleteFavouriteStreetcodeCommand(int Id, int RequestingUserId, UserRole UserRole)
+    : IRequest<Result<FavouriteStreetcodeDTO>>;
