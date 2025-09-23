0e1621dafe715f0a237c47ed56f7580cc8efa667
Valentyn Humen <valentin34561@gmail.com>
2025-09-23 12:51:28 +0300

    Add Dtos for Change User Password

---
+++ Streetcode/Streetcode.BLL/DTO/Users/ChangePassword/ChangePasswordResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.DTO.Users.ChangePassword
+{
+    public class ChangePasswordResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+    }
+}
