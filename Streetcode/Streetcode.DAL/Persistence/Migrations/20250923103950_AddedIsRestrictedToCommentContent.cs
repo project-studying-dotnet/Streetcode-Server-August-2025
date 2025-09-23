dbb64bc475f95443fbb1fea2198c25fa50c3246f
denys-pavskyi <den.pavski@gmail.com>
2025-09-23 14:04:11 +0300

    feat: Updated migration to have IsResctricted in CommentContent

---
+++ Streetcode/Streetcode.DAL/Persistence/Migrations/20250923103950_AddedIsRestrictedToCommentContent.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streetcode.DAL.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsRestrictedToCommentContent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReviewed",
+                schema: "comment",
+                table: "comments");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRestricted",
+                schema: "comment",
+                table: "comments",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRestricted",
+                schema: "comment",
+                table: "comments");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReviewed",
+                schema: "comment",
+                table: "comments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
